bb6b30d3c0a69ea9dd392f4fe8d9dfba87b1e7fb
Thomas Seng Hin Mak <makzan@42games.net>
2015-05-21 13:24:09 +0000

    Updated step 3

--- ios-app/kitchen-timer/step3-local-notification.html.pm
+++ ios-app/kitchen-timer/step3-local-notification.html.pm
@@ -8,8 +8,6 @@ We use LocalNotification to schedule the time up message.
   ◊figure["https://dl.dropboxusercontent.com/u/3079250/Public%20for%20MobileDesignClass/ios/Screen%20Shot%202015-05-18%20at%2012.19.57%20PM.png"]{Prompting user for allowing notification.}
 }
 
-◊figure["https://dl.dropboxusercontent.com/u/3079250/Public%20for%20MobileDesignClass/ios/IMG_0274.jpg"]{Properties for notification action.}
-
 The usage of local notification:
 
 ◊swift{
@@ -96,8 +94,37 @@ The usage of local notification:
   }
 }
 
+◊section{What just happened?}
 
-We have completed the basic local notification functionality. In next step, we’ll work on the ◊em{+30s} and ◊em{-30s} buttons.
+We just scheduled a local notification that alerts in exactly 30 seconds. The alert time is calculate by system to ensure its precision. Another benefit to use notification is that it allows the user to switch to another app during the counting. The user can even kill the app or restart the iPhone. This is a system level scheduling that the alert triggers except the app get deleted or the device is shut down.
+
+
+We can add extra logic to notification by providing categories, which each ◊em{category} contains a couple of actions.
+
+◊sidenote{
+  ◊figure["https://dl.dropboxusercontent.com/u/3079250/Public%20for%20MobileDesignClass/ios/IMG_0274.jpg"]{Properties for notification action.}
+}
+
+There are several properties that we define an ◊em{action}. Here is the code we wrote:
+
+◊swift{
+  action1.identifier = "EXTRA_30S"
+  action1.title = "+30s"
+  action1.activationMode = UIUserNotificationActivationMode.Background
+  action1.destructive = false
+  action1.authenticationRequired = false
+}
+
+◊terms{
+  ◊term["identifier"]{A unique string that the system identifies the action.}
+  ◊term["title"]{The string that is visible to user.}
+  ◊term["activationMode"]{Define if the logic needs to bring the app to ◊em{foreground} or can keep the app executes the logic in ◊em{background}.}
+  ◊term["destructive"]{A ◊em{true} value gives the action button a ◊em{Red} background.}
+  ◊term["authenticationRequired"]{Can user executes this action logic without unlocking the iPhone?}
+}
+
+
+We will handle the action with our customize logic later. Now we have completed the basic local notification functionality. In next step, we’ll work on the ◊em{+30s} and ◊em{-30s} buttons.
 
 ◊btw{
   ◊item{There is no difference in user’s view for ◊em{remote notification} and ◊em{local notification}}
